264a98a0d463d1dc422c4677f375f05bcf2aa031
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-12-01 15:12:40 -0500

    designed employee page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SignUp from "./components/Signup";
+import Login from "./components/Login";
 import reportWebVitals from "./reportWebVitals";
+import Employee from "./components/Employee";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Employee />
   </React.StrictMode>
 );
 
