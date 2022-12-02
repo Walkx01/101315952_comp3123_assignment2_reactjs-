cc79af8f10775c5bb1b1cee23ebf4ed8cee52f83
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-12-01 20:32:39 -0500

    added basic url routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
 import Employee from "./components/Employee";
@@ -8,9 +9,15 @@ import AddEmployee from "./components/AddEmployee";
 
 function App() {
   return (
-    <>
-      <AddEmployee />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route index element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="/employees" element={<Employee />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
